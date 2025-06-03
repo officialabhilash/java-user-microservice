83fc10f550371cfaf2eea00bc7fa034eb40a6ae2
Abhilash <theak.92@gmail.com>
2025-06-03 19:52:14 +0530

    renamed main file for user service

---
+++ src/main/java/com/example/user/UserApplication.java
@@ -0,0 +1,22 @@
+package com.example.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+public class UserApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication app = new SpringApplication(UserApplication.class);
+
+		// Hardcode the external config location
+		Map<String, Object> defaultProps = new HashMap<>();
+		defaultProps.put("spring.config.location", "file:/home/abhilash/Desktop/learning/learn-java/microservices/user-microservice/src/main/resources/application.properties");
+		app.setDefaultProperties(defaultProps);
+		app.run(args);
+	}
+}
